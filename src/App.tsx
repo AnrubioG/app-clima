6365eda0f8f7dce0a18aa3aa13b7a74fbe084106
AnubioG <rubioporrasangela@gmail.com>
2024-09-10 16:40:39 -0500

    configuración de estilos globales

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <>
+      <h1 className={styles.title}>Buscador del clima</h1>
+      <div className={styles.container}>
+        <p>1</p>
+        <p>1</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
