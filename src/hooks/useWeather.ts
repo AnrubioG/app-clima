047e6160eede54d3bf546f742b7c9cd47d77d092
AnubioG <rubioporrasangela@gmail.com>
2024-09-11 18:46:53 -0500

    Corrección geoUrl

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -34,7 +34,7 @@ export function useWeather() {
     setLoading(true);
     setWeather(initalState);
     try {
-      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
+      const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
       const { data } = await axios(geoUrl);
 
       if (!data[0]) {
